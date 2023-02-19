1638d0df96fac0eb1bdb544e14b514057493d7bf
Dan Galbraith <dan@eidria.com>
2023-02-19 11:28:13 -0800

    Initial Commit

---
+++ HitTest/HitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitTestApp.swift
+//  HitTest
+//
+//  Created by Dan Galbraith on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct HitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
